d5343edf7c3b5c13a5f36613b5d594b77033566d
Max Kellermann <max@duempel.org>
2016-04-28 22:20:08 +0200

    Makefile.in, bootstrap.mk: use @srcdir@
    Another out-of-tree build fix.

--- bootstrap.mk
+++ bootstrap.mk
@@ -1,19 +1,21 @@
+srcdir ?= .
+
 BOOTSTRAP_SUBDIRS += configure.ac
 BOOTSTRAP_SUBDIRS += lib/cpluff/configure.ac
 BOOTSTRAP_SUBDIRS += lib/gtest/configure.ac
 
-BOOTSTRAP_TARGETS=$(basename $(BOOTSTRAP_SUBDIRS))
+BOOTSTRAP_TARGETS=$(addprefix $(srcdir)/,$(basename $(BOOTSTRAP_SUBDIRS)))
 all: $(BOOTSTRAP_TARGETS)
 
 %: %.ac
 	autoreconf -vif $(@D)
 	-@rm -rf $(@D)/autom4te.cache
 
-%: %.in
+%: $(srcdir)/%.in
 	autoreconf -vif $(@D)
 	-@rm -rf $(@D)/autom4te.cache
 
-configure: configure.ac
+$(srcdir)/configure: $(srcdir)/configure.ac
 	autoreconf -vif $(@D)
 	-@rm -rf $(@D)/autom4te.cache
 	@test -n "$$BOOTSTRAP_STANDALONE" || ( echo "Configuration is stale. You should almost certainly reconfigure" && false )
