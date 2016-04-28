a5fd465924402d6b6fd9eadfa51772bbd34e2de9
Max Kellermann <max@duempel.org>
2016-04-28 22:20:08 +0200

    codegenerator.mk: move JsonSchemaBuilder build call to json-rpc/Makefile
    Build JsonSchemaBuilder at compile time, not at bootstrap time.
    Bootstrap should only generate the build scripts.

--- codegenerator.mk
+++ codegenerator.mk
@@ -18,14 +18,6 @@ else
 DOXY_XML_PATH=$(GENDIR)/doxygenxml
 endif
 
-GENERATED_JSON = $(INTERFACES_DIR)/json-rpc/ServiceDescription.h addons/xbmc.json/addon.xml
-ifeq ($(wildcard $(JSON_BUILDER)),)
-  JSON_BUILDER = $(shell which JsonSchemaBuilder)
-ifeq ($(JSON_BUILDER),)
-  JSON_BUILDER = tools/depends/native/JsonSchemaBuilder/bin/JsonSchemaBuilder
-endif
-endif
-
 GENDIR = $(INTERFACES_DIR)/python/generated
 GROOVY_DIR = $(TOPDIR)/tools/codegenerator/groovy
 
@@ -50,7 +42,7 @@ $(GENDIR)/%.xml: $(INTERFACES_DIR)/swig/%.i $(SWIG) $(JAVA) $(GENERATE_DEPS)
 	mkdir -p $(GENDIR)
 	$(SWIG) -w401 -c++ -o $@ -xml -I$(TOPDIR)/xbmc -xmllang python $<
 
-codegenerated: $(DOXYGEN) $(SWIG) $(JAVA) $(GENERATED) $(GENERATED_JSON)
+codegenerated: $(DOXYGEN) $(SWIG) $(JAVA) $(GENERATED)
 
 $(DOXY_XML_PATH): $(SWIG) $(JAVA)
 	cd $(INTERFACES_DIR)/python; ($(DOXYGEN) Doxyfile > /dev/null) 2>&1 | grep -v " warning: "
@@ -69,16 +61,3 @@ $(SWIG):
 	@echo Swig not found, it will be used if found after configure.
 	@echo This is not necessarily an error.
 	@false
-
-$(GENERATED_JSON): $(JSON_BUILDER)
-	@echo Jsonbuilder: $(JSON_BUILDER)
-	$(MAKE) -C $(INTERFACES_DIR)/json-rpc $(@F)
-
-$(JSON_BUILDER):
-ifeq ($(BOOTSTRAP_FROM_DEPENDS), yes)
-	@echo JsonSchemaBuilder not found. You didn\'t build depends. Check docs/README.\<yourplatform\>
-	@false
-else
-#build json builder - ".." because makefile is in the parent dir of "bin"
-	$(MAKE) -C $(abspath $(@D)/..)
-endif
