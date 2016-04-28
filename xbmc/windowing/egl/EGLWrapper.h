2622ac9887eed940c2f05d4e21bb99ed39ccd47a
Max Kellermann <max@duempel.org>
2016-04-28 22:20:07 +0200

    Revert "[cleanup] wayland"
    This reverts commit cd814471012ed4f986c3905fc6c6eca754e7c50e.
    
    That commit deleted Wayland support completely without describing a
    reason.  But I want to run Kodi on Wayland.

--- xbmc/windowing/egl/EGLWrapper.h
+++ xbmc/windowing/egl/EGLWrapper.h
@@ -64,6 +64,8 @@ public:
   bool GetConfigAttrib(EGLDisplay display, EGLConfig config, EGLint attribute, EGLint *value);
   bool SurfaceAttrib(EGLDisplay display, EGLSurface surface, EGLint  attribute, EGLint  value);
 
+  bool TrustSurfaceSize();
+
   static void* GetProcAddress(const char* function);
 
 private:
